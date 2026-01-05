3f83b611aa159e94e64979142bdc6f7ea294048d
Gurinder Shergill <shergillgurinder22@gmail.com>
2026-01-04 21:08:38 -0500

    validation fix and key name fix

--- api/contact.js
+++ api/contact.js
@@ -3,7 +3,7 @@ import Resend from "resend";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_PUBLISHABLE_KEY
 );
 
 const resend = new Resend(process.env.RESEND_API_KEY);
